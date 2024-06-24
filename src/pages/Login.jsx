8453a29ef72edaeeffee27f716619c70df325dc5
JohnQuimson <john.quimson777@gmail.com>
2024-06-24 17:46:36 +0200

    feat: simple inputs

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -11,6 +11,8 @@ export default function () {
   return (
     <>
       <form onSubmit={handleLogin}>
+        <input type="text" placeholder="email" />
+        <input type="password" placeholder="password" />
         <button>Login</button>
       </form>
     </>
